0c32ec1d2027d6349c6f17fdb29de701f5de055f
YusuffDevOps <yusuffkazeem56@gmail.com>
2024-04-01 15:37:35 -0300

    Added Project Expenses Query

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import MySQLdb
+import locale
 
 app = Flask(__name__)
 
@@ -8,6 +9,9 @@ DB_HOST = 'localhost'
 DB_USER = 'u28'
 DB_NAME = 'u28'
 DB_PASSWORD = 'schoolCLOTHESdeep691'
+
+#Set locale for currency function 
+locale.setlocale( locale.LC_ALL, 'en_CA.UTF-8' )
 
 # App config
 PORT = 50306 #provide a unique integer value instead of XXXX, e.g., PORT = 15657
@@ -29,7 +33,6 @@ def tables():
     if request.method == 'POST':
         #Get the submitted table name from our form
         table_name = request.form['name']
-        print(table_name)
         # execute query to get table elements
         cur.execute(f"SELECT * FROM {table_name}")
         data = cur.fetchall()
@@ -39,7 +42,6 @@ def tables():
         arr =[]
         for c in columns:
             arr.append(c['COLUMN_NAME'])
-        print(arr)
         cur.close()
         conn.close()
         # return to frontend the data in table_name and the corresponding columns in table_name
@@ -70,10 +72,35 @@ def total_expense(start, end):
     part_orders.part_id = parts._id AND orders._id = part_orders.order_id AND 
     YEAR(order_date)>={start} AND YEAR(order_date)<={end} GROUP BY YEAR(order_date);""")
     data = cur.fetchall()
-    print(data)
     cur.close()
     conn.close()
-    return render_template('annual_expense.html', data=data)
+    return render_template('annual_expense.html', data=data, currency=locale.currency)
+
+#Budget Projection
+@app.route("/project-budget", methods=['GET', 'POST'])
+def project_budget():
+    if request.method == "GET":
+        return render_template('project_budget.html', data=[])
+    else:
+        #Get number of years and inflation rate
+        years = int(request.form["numYears"])
+        rate = float(request.form["rate"])
+        arr = []
+        conn = get_db_connection()
+        cur = conn.cursor()
+        #SQL query to get the recent expense 
+        cur.execute("""SELECT YEAR(order_date) as year, sum(price*qty) 
+        FROM part_orders, parts, orders 
+        WHERE part_orders.part_id = parts._id AND orders._id = part_orders.order_id AND YEAR(order_date)=2023 
+        GROUP BY YEAR(order_date);""")
+        data = cur.fetchall()
+        recent_year = data[0][0]
+        recent_expense = data[0][1]
+        total_rate = (100+rate)/100
+        #Add tuple containg new year and projected expenses for the year
+        for i in range (1, years+1):
+            arr.append((recent_year+i, float("{:.2f}".format((recent_expense)*(total_rate)**(i)))))
+        return render_template('project_budget.html', data=arr, currency=locale.currency)
 
 
 if __name__ == '__main__':
