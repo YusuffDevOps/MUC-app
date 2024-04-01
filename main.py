6eed27998c4fd5f8980200ce3ca21b9771cb3895
YusuffDevOps <yusuffkazeem56@gmail.com>
2024-04-01 12:13:03 -0300

    Added Get Expenses Query

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ DB_NAME = 'u28'
 DB_PASSWORD = 'schoolCLOTHESdeep691'
 
 # App config
-PORT = 15097 #provide a unique integer value instead of XXXX, e.g., PORT = 15657
+PORT = 50306 #provide a unique integer value instead of XXXX, e.g., PORT = 15657
 
 
 def get_db_connection():
@@ -47,14 +47,34 @@ def tables():
     else:
         return render_template('show_table.html', data=[], columns=[])
 
-#Add new supplier
-@app.route("/add-supplier", methods=['GET', 'POST'])
-def add_supplier():
+
+#shows expenses page and redirects user to request
+@app.route("/get-expenses/", methods=['GET', 'POST'])
+def get_expenses():
+    if request.method =='GET':
+        return render_template('annual_expense.html', data=[])
+    else:
+        start = request.form["start"]
+        end = request.form["end"]
+        return redirect(f"/get-annual-expense/{start}/{end}")
+
+
+# GET Annual Expenses for parts from start to end year
+@app.route("/get-annual-expense/<string:start>/<string:end>", methods=['GET'])
+def total_expense(start, end):
     conn = get_db_connection()
-    cur = conn.cursor(MySQLdb.cursors.DictCursor)
-    details = request.form
-    #check if supplier already exists
-    #If not add them to the DB
+    cur = conn.cursor()
+    #SQL query to get total expense for each YEAR
+    cur.execute(f"""SELECT YEAR(order_date) as year,  sum(price*qty) FROM 
+    part_orders, parts, orders WHERE 
+    part_orders.part_id = parts._id AND orders._id = part_orders.order_id AND 
+    YEAR(order_date)>={start} AND YEAR(order_date)<={end} GROUP BY YEAR(order_date);""")
+    data = cur.fetchall()
+    print(data)
+    cur.close()
+    conn.close()
+    return render_template('annual_expense.html', data=data)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=PORT)
